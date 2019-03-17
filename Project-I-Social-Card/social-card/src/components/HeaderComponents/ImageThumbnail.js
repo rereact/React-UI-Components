c08f4f1ac9d988cca385ad1e9fcd1379376e8095
JaySebagh <jaysebagh@gmail.com>
2019-03-16 18:17:15 -0700

    created header component

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
@@ -1,2 +1,13 @@
 import React from 'react';
 import './Header.css';
+import LambdaLogo from './LambdaLogo.png'
+
+const ImageThumbnail = () => {
+    return (
+        <div>
+            <img className="logo" src={LambdaLogo} alt="Lambda Logo" />
+        </div>
+    )
+}
+
+export default ImageThumbnail
